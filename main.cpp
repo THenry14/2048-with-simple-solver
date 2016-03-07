35591a05ceb7d28fabb4e6ad535ffbf97a3f4fb6
Łukasz Żarnowiecki <dolohow@outlook.com>
2016-03-07 21:03:09 +0100

    Remove all blank lines

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@ Implementation of 2048 game and its solver
 
 #include "class2048.h"
 #include <iomanip>
-#include <algorithm>																
+#include <algorithm>
 
-using namespace std;									
-  
-int main( int argc, char* argv[] )						
+using namespace std;
+
+int main( int argc, char* argv[] )
 {
-    srand( static_cast<uint>( time( NULL ) ) );			
-    game2048 g; g.start();								
-    return system( "pause" );							
+    srand( static_cast<uint>( time( NULL ) ) );
+    game2048 g; g.start();
+    return system( "pause" );
 }
